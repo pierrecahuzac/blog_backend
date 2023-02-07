01cef36ee591ce6f018f006d9059a6a043b3cb3a
inflames32 <cahuzac.p@gmail.com>
2023-02-07 20:22:41 +0100

    fix fetch error

--- controller/postsController.js
+++ controller/postsController.js
@@ -1,5 +1,8 @@
 const Airtable = require("../config/api");
+
+const fetch = require("node-fetch");
 const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID } = process.env;
+
 const base = new Airtable({
   apiKey: AIRTABLE_API_KEY,
 }).base(AIRTABLE_BASE_ID);

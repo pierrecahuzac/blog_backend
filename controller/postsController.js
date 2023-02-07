872642a791bf9795d740808fe9da1580c37005f0
inflames32 <cahuzac.p@gmail.com>
2023-02-07 20:46:04 +0100

    replace fetch by axios

--- controller/postsController.js
+++ controller/postsController.js
@@ -1,8 +1,6 @@
 const Airtable = require("../config/api");
 const axios = require("axios");
-
 const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID } = process.env;
-
 const base = new Airtable({
   apiKey: AIRTABLE_API_KEY,
 }).base(AIRTABLE_BASE_ID);

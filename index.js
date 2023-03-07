0e5ca7cb43bcf009bdbe3600acf083e0bc37763e
inflames32 <cahuzac.p@gmail.com>
2023-03-07 23:44:54 +0100

    fix cors

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const router = require("./router");
 const app = express();
 const cors = require("cors");
 
-const whiteListOrigin = [
+/* const whiteListOrigin = [
   /*   process.env.CLIENT_URL,
-  process.env.FRONT_PROD_URL, */
+  process.env.FRONT_PROD_URL, 
   "*",
 ];
-
+ */
 // cors management
-var corsOptions = {
+/* var corsOptions = {
   origin: whiteListOrigin,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   preflightContinue: false,
   credentials: true,
 };
-
+ */
 dotenv.config();
-app.use(cors(corsOptions));
+/* app.use(cors({ origin: "*" })); */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
